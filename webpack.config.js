9f9487febf5c91cf594313280334275c02f8fa21
James Robert Huggins Ngo <jamesrobert.ngo@gmail.com>
2019-01-07 01:53:02 -0500

    Add Babel and Babel Loader

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,20 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const path = require('path');
 
 module.exports = {
-
 	entry: path.join(__dirname, 'src/index.js'),
 	module: {
 		rules: [{
 			test: /\.css$/,
 			use: ['style-loader', 'css-loader']
+		}, {
+			test: /\.m?js$/,
+			exclude: /(node_modules|bower_components)/,
+			use: {
+				loader: 'babel-loader',
+				options: {
+					presets: ['babel-preset-env']
+				}
+			}
 		}]
 	},
 	output: {
